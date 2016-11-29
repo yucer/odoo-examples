de961ce8be76cd8f85f42be3a2aed97a6f891c30
Yurdik Cervantes Mendoza <ycervantes@elegosoft.com>
2016-11-29 14:25:21 +0100

    adds module for migration_test v1.0

---
+++ migration_test/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Migration tests',
+    'summary': 'Test module for special migration scripts',
+    'version': '0.1',
+    'author': 'Elegosoft GmbH',
+    'depends': [
+    ],
+    'data': [],
+    'demo': [
+    ],
+    'installable': True,
+    'application': False,
+    'description': """""",
+}
